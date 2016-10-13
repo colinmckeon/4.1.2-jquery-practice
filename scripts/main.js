92f72c1fdf8d58ef8bf49393b8016b14b78f844a
Colin McKeon <14mckeon@gmail.com>
2016-10-12 20:49:24 -0400

    added divs around blocks

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,23 @@
   $(function () {
     "use strict";
 
+    var $heading = $('.heading');
+
+
+    $heading.on('click', function(){
+        var target = $(this);
+
+
+    })
 
 
 
 
   });       /* End of the global functions */
 }(jQuery));
+
+
+// if(target.next().is(:visible)) {
+//   target.next().slideUp();
+// }else{
+//   target.next().slideDown();
